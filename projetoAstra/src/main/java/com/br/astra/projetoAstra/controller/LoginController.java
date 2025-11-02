ba8e8856b2a922910eda4057e5b859e1e81db5bc
EduuhSouza <146948069+EduuhSouza@users.noreply.github.com>
2025-11-02 14:53:05 -0300

    Add files via upload

--- projetoAstra/src/main/java/com/br/astra/projetoAstra/controller/LoginController.java
+++ projetoAstra/src/main/java/com/br/astra/projetoAstra/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.br.astra.projetoAstra.controller;
 
-
 import com.br.astra.projetoAstra.model.User;
 import com.br.astra.projetoAstra.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +22,22 @@ public class LoginController {
 
     @Autowired
     private UserRepository ur;
+
+    @PostMapping("/logar")
+    public String loginUser(User user, Model model, HttpServletResponse response){
+        User userLogado = this.ur.login(user.getEmail(), user.getPassword());
+        if (userLogado != null){
+            return "redirect:/";
+        }
+
+        model.addAttribute("erro", "Usuário inválido");
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String dashboard(){
+        return "dashboard";
+    }
 
     @GetMapping("/index")
     public String login(){
@@ -31,9 +51,7 @@ public class LoginController {
         if (result.hasErrors()){
             return "redirect:/index";
         }
-
         ur.save(user);
-
         return "redirect:/index";
     }
 
--- projetoAstra/src/main/java/com/br/astra/projetoAstra/model/User.java
+++ projetoAstra/src/main/java/com/br/astra/projetoAstra/model/User.java
@@ -9,23 +9,48 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
     private long id;
 
     @NotEmpty
-    @Getter @Setter
     private String username;
 
     @NotEmpty
-    @Getter @Setter
     private String email;
 
     @NotEmpty
-    @Getter @Setter
     private String password;
 
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
